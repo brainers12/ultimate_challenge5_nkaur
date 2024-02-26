1854f5e6f4452f63f0cafac5b7529f8801d53559
Navjot <navjotcheema289@gmail.com>
2024-02-25 18:32:33 -0600

    Add show action and view for Product, update index with links

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+
   # root "posts#index"
+
+  resources :products, only: [:index, :show]
+
 end
